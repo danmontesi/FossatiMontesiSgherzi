4be775cf03e344556ac79b80326db745e3edf655
lnghrdntcf <francesco.sgherzi@gmail.com>
2019-01-07 20:52:15 +0100

    first version of functional Individual Manager

--- IMPLEMENTATION/backend/__tests__/indiv/featureTest.jest.js
+++ IMPLEMENTATION/backend/__tests__/indiv/featureTest.jest.js
@@ -52,4 +52,96 @@ describe('User sends data', () => {
     expect(res.success).toBe(true)
     expect(res.message).toBe('Sync successful')
   })
+  test('User sends data formatted badly', async () => {
+    let res = await fetch(LOCAL_BASE_URL + 'indiv/data', {
+      method: 'POST',
+      headers: new fetch.Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({
+        auth_token: userToken,
+        data: {
+          gps_coordinates: [
+            {
+              lat: 45.4772669,
+              long: 9.2343508,
+              timestamp: new Date()
+            }
+          ],
+          accelerometer: [
+            {
+              timestamp: new Date(2014, 0, 1),
+              acc_x: -2,
+              acc_y: 2.123,
+              acc_z: 141
+            }, {
+              timestamp: new Date(),
+              acc_x: -12,
+              acc_y: 5.123,
+              acc_z: 11
+            }
+          ],
+          heart_rate: [
+            {
+              timestamp: new Date(2014, 0, 1),
+              bpm: 80
+            }, {
+              timestamp: new Date(),
+              bpm: 84
+            }
+          ]
+        }
+      })
+    })
+    res = await res.json()
+    console.log(res)
+    expect(res.success).toBe(true)
+    expect(res.message).toBe('Sync successful')
+  })
+})
+
+describe('User wants to retrive his data', () => {
+  test('Data retrival', async () => {
+    let res = await fetch(LOCAL_BASE_URL + 'indiv/data' + '?auth_token=' +
+      userToken +
+      '&begin_date=' + new Date(1970, 0, 0).toISOString() + '&end_date=' + new Date(2014, 0, 1).toISOString(), {
+      method: 'GET',
+      headers: new fetch.Headers({
+        'Content-Type': 'application/json'
+      })
+    })
+    res = await res.json()
+    console.log(res)
+    expect(res.success).toBe(true)
+    expect(res.data).not.toBe(null)
+  })
+
+  test('Data retrival - Malformed', async () => {
+    let res = await fetch(LOCAL_BASE_URL + 'indiv/data' + '?auth_token=' +
+      userToken +
+      '&begin_date=' + new Date(1970, 0, 0).toISOString() + '&end_date=' + 'AAAA', {
+      method: 'GET',
+      headers: new fetch.Headers({
+        'Content-Type': 'application/json'
+      })
+    })
+    res = await res.json()
+    expect(res.status).toBe(500)
+  })
+})
+
+describe('User wants to retrive it\'s information',  () => {
+  test('', async () => {
+    let res = await fetch(LOCAL_BASE_URL + 'indiv/user?'
+      + 'auth_token=' + userToken,
+      {
+        method: 'GET',
+        headers: new fetch.Headers({
+          'Content-Type': 'application/json'
+        })
+      })
+    res = await res.json()
+    expect(res.success).toBe(true)
+    expect(res.user).not.toBe(null)
+  })
 })
--- IMPLEMENTATION/backend/managers/individual/FunctionalIndividualsManager.js
+++ IMPLEMENTATION/backend/managers/individual/FunctionalIndividualsManager.js
@@ -13,6 +13,12 @@ const {
   connect
 } = require('../config')
 
+/**
+ * Allows to save the data of the given user in the database
+ * @param auth_token: String
+ * @param bodyData: JSON The data to be saved
+ * @returns {Promise<{success: boolean, message: string}>}
+ */
 async function saveData(auth_token, bodyData) {
 
   // As sometimes the parser fails to recognize that the data are JSON
@@ -31,33 +37,33 @@ async function saveData(auth_token, bodyData) {
 
   // Connect to the pool
   const client = await connect()
-
   try {
     await client.query('BEGIN')
 
+    // Inserts into the tables every data received
     gps_coordinates.forEachAsync(async (coordinate) => {
-      client.query('INSERT INTO gps_coordinates(user_id, timestamp, lat, long) VALUES($1, $2, $3, $4) RETURNING *', [user.id, coordinate.timestamp, coordinate.lat, coordinate.long])
+      await client.query('INSERT INTO gps_coordinates(user_id, timestamp, lat, long) VALUES($1, $2, $3, $4) RETURNING *', [user.id, coordinate.timestamp, coordinate.lat, coordinate.long])
     })
 
     accelerometer.forEachAsync(async (accData) => {
-      client.query('INSERT INTO accelerometer(user_id, timestamp, acc_x, acc_y, acc_z) VALUES($1, $2, $3, $4, $5) RETURNING *', [user.id, accData.timestamp, accData.acc_x, accData.acc_y, accData.acc_z])
+      await client.query('INSERT INTO accelerometer(user_id, timestamp, acc_x, acc_y, acc_z) VALUES($1, $2, $3, $4, $5) RETURNING *', [user.id, accData.timestamp, accData.acc_x, accData.acc_y, accData.acc_z])
     })
 
     heart_rate.forEachAsync(async (heartData) => {
-      client.query('INSERT INTO heart_rate(user_id, timestamp, bpm) VALUES($1, $2, $3) RETURNING *', [user.id, heartData.timestamp, heartData.bpm])
+      await client.query('INSERT INTO heart_rate(user_id, timestamp, bpm) VALUES($1, $2, $3) RETURNING *', [user.id, heartData.timestamp, heartData.bpm])
     })
 
     await client.query('COMMIT')
     await client.release()
 
     // TODO: NOTIFY COMPANIES
+
     return {
       success: true,
       message: 'Sync successful'
     }
 
   } catch (err) {
-    console.log(err)
     await client.query('ROLLBACK')
     await client.release()
     err.message = 'Invalid data'
@@ -67,9 +73,17 @@ async function saveData(auth_token, bodyData) {
 
 }
 
+/**
+ * Allows the client to retrive its data from the database
+ * @param userId: Integer
+ * @param fromDate: Date
+ * @param toDate: Date
+ * @param isIndividualQuery: Boolean If this function is called in the context of an individual query
+ * @returns {Promise<{data}>}
+ */
 async function getData(userId, fromDate = undefined, toDate = undefined, isIndividualQuery = false) {
 
-  const client = await IndividualsManager.connect()
+  const client = await connect()
 
   let templateEnd = ''
   let paramsEnd = []
@@ -102,7 +116,52 @@ async function getData(userId, fromDate = undefined, toDate = undefined, isIndiv
 
 }
 
+/**
+ * Allows to retrive user info
+ * @param token: String
+ * @returns {Promise<{success: boolean, user: *}>}
+ */
+  async function getUserInfo(token) {
+
+  // Get the user_id
+  const {
+    id
+  } = getActor(token)
+
+  // Connect to the pool
+  const client = await connect()
+
+  try {
+
+    const {
+      rows
+    } = await client.query('SELECT * FROM individual_account WHERE id = $1', [id])
+
+    if (rows.length === 0) {
+      let err = new Error('User not found')
+      err.status = 404
+      throw err
+    }
+
+    // Don't send internal id and password
+    rows[0].id = undefined
+    rows[0].password = undefined
+
+    return {
+      success: true,
+      user: rows[0]
+    }
+
+  } catch (err) {
+    console.log(err)
+    await client.release()
+    throw err
+  }
+
+}
+
 module.exports = {
   saveData,
-  getData
+  getData,
+  getUserInfo
 }
